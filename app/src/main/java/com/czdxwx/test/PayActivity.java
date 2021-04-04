743161deff751db7af52affc5153e5e2b5704060
CZDXWX1226566881 <1226566881@qq.com>
2021-04-04 11:34:03 +0800

    第三版

--- app/src/main/java/com/czdxwx/test/PayActivity.java
+++ app/src/main/java/com/czdxwx/test/PayActivity.java
@@ -1,14 +1,17 @@
 package com.czdxwx.test;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -19,6 +22,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.czdxwx.test.model.Major;
 import com.czdxwx.test.model.Person;
@@ -37,15 +41,33 @@ public class PayActivity extends AppCompatActivity {
     private TextView pay10;
     private TextView pay20;
     private TextView pay30;
-
+    private Toolbar mToolbar;
     private int last = -1;
     private Button btn_pay;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
-
+        //隐藏默认actionbar
+        ActionBar actionBar = this.getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        //获取toolbar
+        mToolbar = findViewById(R.id.pay_toolbar);
+        //主标题，必须在setSupportActionBar之前设置，否则无效，如果放在其他位置，则直接setTitle即可
+        mToolbar.setTitle("支付");
+        //用toolbar替换actionbar
+        setActionBar(mToolbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);//显示toolbar的返回按钮
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         db = FinalDb.create(this, "test");
         number = findViewById(R.id.pay_number);
         btn_pay = findViewById(R.id.btn_pay);
@@ -122,88 +144,88 @@ public class PayActivity extends AppCompatActivity {
                 // 从连接中获取Stub对象
                 payService = PayService.Stub.asInterface(iBinder);
                 // 调用Remote Service提供的方法
-                    AlertDialog.Builder builder = new AlertDialog.Builder(PayActivity.this);
-                    builder.setTitle("请输入支付密码:");
-                    input = new EditText(PayActivity.this);
-                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.MATCH_PARENT);
-                    input.setLayoutParams(lp);
-                    builder.setView(input);
-                    builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            DbModel dbModel = db.findDbModelBySQL(String.format("select * from person where number='%s'", number.getText()));
-                            if(input.getText().toString().equals(dbModel.getString("password"))){
-                                balance = dbModel.getFloat("balance");
-                                float state = 0;
-                                try {
-                                    state = payService.pay(money, balance);
-                                } catch (RemoteException e) {
-                                    e.printStackTrace();
-                                }
-                                AlertDialog.Builder builder = new AlertDialog.Builder(PayActivity.this);
-                                if (state == -1) {
-                                    builder.setTitle("支付失败");
-                                    builder.setMessage("余额不足");
-                                } else {
-                                    Person info = new Person();
-                                    info.setName(dbModel.getString("name"));
-                                    info.setNumber(dbModel.getString("number"));
-                                    info.setGender(dbModel.getString("gender"));
-                                    info.setHobby(dbModel.getString("hobby"));
-                                    info.setNativePlace(dbModel.getString("nativePlace"));
-                                    info.setMajorid(dbModel.getInt("majorid"));
-                                    info.setIsOnFocus(dbModel.getInt("isOnFocus"));
-                                    info.setProfile(dbModel.getString("profile"));
-                                    info.setPassword(dbModel.getString("password"));
-                                    info.setBalance(state);
-                                    db.update(info, String.format("number='%s'", info.getNumber()));
-                                    builder.setTitle("支付成功");
-                                    builder.setMessage("余额为" + state);
-                                }
-                                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                                AlertDialog alert = builder.create();
-                                alert.show();
-                            }else{
-                                AlertDialog.Builder builder = new AlertDialog.Builder(PayActivity.this);
-                                builder.setTitle("密码错误");
-                                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                                AlertDialog alert = builder.create();
-                                alert.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(PayActivity.this);
+                builder.setTitle("请输入支付密码:");
+                input = new EditText(PayActivity.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                builder.setView(input);
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DbModel dbModel = db.findDbModelBySQL(String.format("select * from person where number='%s'", number.getText()));
+                        if (input.getText().toString().equals(dbModel.getString("password"))) {
+                            balance = dbModel.getFloat("balance");
+                            float state = 0;
+                            try {
+                                state = payService.pay(money, balance);
+                            } catch (RemoteException e) {
+                                e.printStackTrace();
                             }
+                            AlertDialog.Builder builder = new AlertDialog.Builder(PayActivity.this);
+                            if (state == -1) {
+                                builder.setTitle("支付失败");
+                                builder.setMessage("余额不足");
+                            } else {
+                                Person info = new Person();
+                                info.setName(dbModel.getString("name"));
+                                info.setNumber(dbModel.getString("number"));
+                                info.setGender(dbModel.getString("gender"));
+                                info.setHobby(dbModel.getString("hobby"));
+                                info.setNativePlace(dbModel.getString("nativePlace"));
+                                info.setMajorid(dbModel.getInt("majorid"));
+                                info.setIsOnFocus(dbModel.getInt("isOnFocus"));
+                                info.setProfile(dbModel.getString("profile"));
+                                info.setPassword(dbModel.getString("password"));
+                                info.setBalance(state);
+                                db.update(info, String.format("number='%s'", info.getNumber()));
+                                builder.setTitle("支付成功");
+                                builder.setMessage("余额为" + state);
+                            }
+                            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                            AlertDialog alert = builder.create();
+                            alert.show();
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(PayActivity.this);
+                            builder.setTitle("密码错误");
+                            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                            AlertDialog alert = builder.create();
+                            alert.show();
                         }
-                    });
-                    builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    });
-                    AlertDialog alert = builder.create();
-                    alert.show();
+                    }
+                });
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
 
 
             }
--- app/src/main/java/com/czdxwx/test/model/Payment.java
+++ app/src/main/java/com/czdxwx/test/model/Payment.java
@@ -2,13 +2,38 @@ package com.czdxwx.test.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.text.Editable;
 
 public class Payment implements Parcelable {
     private String username;
     private String number;
-    private int money;
+    private float money;
 
-    protected Payment(Parcel in) {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    public Payment(Parcel in) {
         username = in.readString();
         number = in.readString();
         money = in.readInt();
@@ -26,6 +51,11 @@ public class Payment implements Parcelable {
         }
     };
 
+    public Payment() {
+
+    }
+
+
     @Override
     public int describeContents() {
         return 0;
@@ -35,6 +65,6 @@ public class Payment implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(username);
         dest.writeString(number);
-        dest.writeInt(money);
+        dest.writeFloat(money);
     }
 }
